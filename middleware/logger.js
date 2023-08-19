7fd2023193e80d0b98c208aa3fc71ab91621a524
shaaibu <shaaibusuleiman@gamil.com>
2023-08-19 14:51:55 +0100

    complete server logs functionlity

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,4 +1,4 @@
-const { format } = require('data-fns');
+const { format } = require('date-fns');
 const { v4: uuid } = require('uuid');
 const fs = require('fs');
 const fsPromises = require('fs').promises;
