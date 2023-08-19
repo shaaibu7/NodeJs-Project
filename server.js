7256050c97c258c561248cc7a7c68e31efd785c5
shaaibu <shaaibusuleiman@gamil.com>
2023-08-19 14:38:15 +0100

    setup middlewares and server logs

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+
 app.use('/', express.static(path.join(__dirname, '/public')));
 
 app.use('/', require('./routes/root'));
